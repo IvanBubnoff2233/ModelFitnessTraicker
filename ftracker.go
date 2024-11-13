61011822dca5a605e4510e8572fe3eeb9c3f66dc
Ivan Bubnov <bubnov.ivanbubnoff@yandex.ru>
2024-11-13 06:43:23 +0300

    Adding corrected file

--- ftracker.go
+++ ftracker.go
@@ -105,7 +105,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	speedMsec := meanSpeed(action, duration) / 3.6
+	speedMsec := meanSpeed(action, duration) * kmhInMsec
 
 	calories := ((walkingCaloriesWeightMultiplier*weight + (math.Pow(speedMsec, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 
